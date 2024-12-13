2e33e2c8ae6c96aef11544cb93762f6ec09a29e2
Artyom_Kazagashev <tema124ru@mail.ru>
2024-12-13 19:54:51 +0700

    Шаблонизация

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,12 +5,8 @@ from catalog.views import home, contacts, products_list, product_detail
 app_name = CatalogConfig.name
 
 urlpatterns = [
-
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('products_list/', products_list, name='products_list'),
-    path('product_detail/<int:pk>/', product_detail, name='product_detail'),
+    path('product_list/', products_list, name='products_list'),
+    path('product_detail/<int:pk/', product_detail, name='product_detail'),
 ]
-
-
-
